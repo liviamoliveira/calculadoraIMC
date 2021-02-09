912560498f31547a345ed83ff38e2976ec5f2758
liviamoliveira <liviamaria.s.oliveira@hotmail.com>
2021-02-09 19:18:12 -0300

    Implementacao calculo e layouts

--- app/src/main/java/dev/estudos/calculadoraimc/resultado/ResultadoPesoActivity.kt
+++ app/src/main/java/dev/estudos/calculadoraimc/resultado/ResultadoPesoActivity.kt
@@ -2,6 +2,7 @@ package dev.estudos.calculadoraimc.resultado
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import dev.estudos.calculadoraimc.R
 import kotlinx.android.synthetic.main.activity_resultado_peso.*
 
@@ -16,19 +17,42 @@ class ResultadoPesoActivity : AppCompatActivity() {
         viewModel = ResultadoPesoViewModel()
 
         setContentView(R.layout.activity_resultado_peso)
-        setupInicial()
         parametrosIniciais()
-    }
-
-    fun setupInicial(){
-
     }
 
     fun parametrosIniciais(){
         val resultadoPeso = intent.getStringExtra(PARAM_PESO) ?: "0.00"
-
         tvValor.text = resultadoPeso
+        verificarIMC(resultadoPeso.replace(",", ".").toDouble())
+    }
 
+    fun verificarIMC(resultPesoDouble: Double) {
+        if (resultPesoDouble < 17.00) {
+            tvValor.setTextColor(ContextCompat.getColor(this,R.color.red))
+            tvMsgDinamica.setTextColor(ContextCompat.getColor(this,R.color.red))
+            tvAvaliacao.text = "CUIDADO!!!!"
+            tvMsgDinamica.text = "CUIDADO!!! Você está num nível alto de Magreza."
+        } else if (resultPesoDouble == 17.0 && resultPesoDouble < 18.5) {
+            tvValor.setTextColor(ContextCompat.getColor(this,R.color.yellow))
+            tvMsgDinamica.setTextColor(ContextCompat.getColor(this,R.color.yellow))
+            tvAvaliacao.text = "ATENÇÃO!!!!"
+            tvMsgDinamica.text = "ATENÇÃO!!! Você está abaixo do peso."
+        } else if (resultPesoDouble == 18.5 && resultPesoDouble < 25.0) {
+            tvValor.setTextColor(ContextCompat.getColor(this,R.color.green_app))
+            tvMsgDinamica.setTextColor(ContextCompat.getColor(this,R.color.green_app))
+            tvAvaliacao.text = "PARABÉNS!!!!"
+            tvMsgDinamica.text = "PARABÉNS!!! Você está dentro do peso ideal."
+        } else if (resultPesoDouble == 25.0 && resultPesoDouble <= 30.0) {
+            tvValor.setTextColor(ContextCompat.getColor(this,R.color.yellow))
+            tvMsgDinamica.setTextColor(ContextCompat.getColor(this,R.color.yellow))
+            tvAvaliacao.text = "ATENÇÃO!!!!"
+            tvMsgDinamica.text = "ATENÇÃO!!! Você está com sobrepreso."
+        } else {
+            tvValor.setTextColor(ContextCompat.getColor(this,R.color.red))
+            tvMsgDinamica.setTextColor(ContextCompat.getColor(this,R.color.red))
+            tvAvaliacao.text = "CUIDADO!!!!"
+            tvMsgDinamica.text = "CUIDADO!!! Você está obeso."
+        }
     }
 
     companion object{
